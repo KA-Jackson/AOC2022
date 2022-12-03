59835511d21d80012df31fc632eebd89ce2bcd41
Kevin Jackson <kjacksona7@gmail.com>
2022-12-03 16:39:36 +0000

    Setup day 3

---
+++ day_03/day3.py
@@ -0,0 +1,4 @@
+input = 'day_03\\input_day3.txt'
+
+def puzzle_5(filename):
+    return 0
---
+++ day_03/test_day3.py
@@ -0,0 +1,7 @@
+import day3
+
+test_input = 'day_03\\test_input_day3.txt'
+input = 'day_03\\input_day3.txt'
+
+def test_puzzle5():
+    assert day3.puzzle_5(test_input) == 157
